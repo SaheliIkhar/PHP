33a9a26ddd57bbeb802df5a000e2549d07bf0e4e
Saheli <saheliiikhar@avestantechnologies.in>
2022-09-15 11:21:36 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post">
+Enter the Number:
+<input type="number"name="number">
+<input type="submit"value="Submit">
+</form>
+</body>
+</html>
+<?php>
+if($_POST){
+$number=$_POST['number'];
+if(($number%2)==0){
+echo"$number is an Even number";
+}
+else{
+ echo"$number is  Odd number";
+}
+}
+?>
